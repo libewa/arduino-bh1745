d62389ce2c8c331a80a11e06078423b767a4ce6f
libewa <libewa.git@icloud.com>
2025-06-06 15:45:43 +0200

    steal i2c code part 1

--- BH1745.cpp
+++ BH1745.cpp
@@ -5,262 +5,263 @@
 #include <Wire.h>
 
 // Register addresses
-#define SYSTEM_CONTROL  0x40
-#define MODE_CONTROL1   0x41
-#define MODE_CONTROL2   0x42
-#define MODE_CONTROL3   0x44
-#define COLOUR_DATA     0x50
-#define DINT_DATA       0x58
-#define INTERRUPT       0x60
-#define PERSISTENCE     0x61
-#define THRESHOLD_LOW   0x64
-#define THRESHOLD_HIGH  0x62
-#define MANUFACTURER    0x92
+#define SYSTEM_CONTROL 0x40
+#define MODE_CONTROL1 0x41
+#define MODE_CONTROL2 0x42
+#define MODE_CONTROL3 0x44
+#define COLOUR_DATA 0x50
+#define DINT_DATA 0x58
+#define INTERRUPT 0x60
+#define PERSISTENCE 0x61
+#define THRESHOLD_LOW 0x64
+#define THRESHOLD_HIGH 0x62
+#define MANUFACTURER 0x92
 
 // Bit masks and settings
-#define SW_RESET        0b01000000
-#define INT_RESET       0b00100000
-#define PART_ID_MASK    0b00001111
+#define SW_RESET 0b01000000
+#define INT_RESET 0b00100000
+#define PART_ID_MASK 0b00001111
 
-#define PART_ID         0b001011
+#define PART_ID 0b001011
 #define MANUFACTURER_ID 0xE0
 
 void BH1745::begin() {
-    Wire.begin();
+  Wire.begin();
 #ifdef BH1745_DEBUG
-    Serial.begin(9600);
-    Serial.println("This is arduino-BH1745 debug mode!");
+  Serial.begin(9600);
+  Serial.println("This is arduino-BH1745 debug mode!");
 #endif
 }
 
 BH1745::BH1745(uint8_t address) {
-    address = address;
+  this->address = address;
 
-    setWhiteBalance(2.2, 1.0, 1.8, 10.0);
-    enableWhiteBalance = true;
+  setWhiteBalance(2.2, 1.0, 1.8, 10.0);
+  enableWhiteBalance = true;
 
-    modeControl2.valid = 0;
-    modeControl2.rgb_en = 1;
-    modeControl2.adc_gain = 0b00;
-
+  modeControl2.valid = 0;
+  modeControl2.rgb_en = 1;
+  modeControl2.adc_gain = 0b00;
 }
 
 bool BH1745::init() {
-    // "Power on time: t1: t1 should be more than 2ms ..."
-    delay(5);
-    reset(); // safety
+  // "Power on time: t1: t1 should be more than 2ms ..."
+  delay(5);
+  reset();  // safety
 
-    uint8_t part_id = readRegister(SYSTEM_CONTROL) & PART_ID_MASK;
-    uint8_t manufacturer_id = readRegister(MANUFACTURER);
-    if (part_id != PART_ID || manufacturer_id != MANUFACTURER_ID) {
-        Serial.println("BH1745 not found: Manufacturer or Part ID mismatch!");
-        return false;
-    }
+  uint8_t part_id = readRegister(SYSTEM_CONTROL) & PART_ID_MASK;
+  uint8_t manufacturer_id = readRegister(MANUFACTURER);
+  if (part_id != PART_ID || manufacturer_id != MANUFACTURER_ID) {
+    Serial.println("BH1745 not found: Manufacturer or Part ID mismatch!");
+    return false;
+  }
 
-    reset();
-    setBit(SYSTEM_CONTROL, 6, false);
+  reset();
+  setBit(SYSTEM_CONTROL, 6, false);
 
-    setMeasurementTime(640);
-    writeRegister(MODE_CONTROL2, makeModeControl2());
-    writeRegister(MODE_CONTROL3, 0x02); // Copied from documentation
-    
-    setThresholdHigh(0);
-    setThresholdLow(0xFFFF);
-    setBit(INTERRUPT, 4, false);
-    delay(320);
-    return true;
+  setMeasurementTime(640);
+  writeRegister(MODE_CONTROL2, makeModeControl2());
+  writeRegister(MODE_CONTROL3, 0x02);  // Copied from documentation
+
+  setThresholdHigh(0);
+  setThresholdLow(0xFFFF);
+  setBit(INTERRUPT, 4, false);
+  delay(320);
+  return true;
 }
 
 void BH1745::setBit(uint8_t reg, uint8_t bit, bool newValue) {
-    uint8_t val = readRegister(reg);
-    uint8_t mask = ~(1 << bit);
-    val &= mask;
-    if (newValue == true) {
-        val |= 1 << bit;
-    }
-    writeRegister(reg, val);
+  uint8_t val = readRegister(reg);
+  uint8_t mask = ~(1 << bit);
+  val &= mask;
+  if (newValue == true) {
+    val |= 1 << bit;
+  }
+  writeRegister(reg, val);
 }
 
 bool BH1745::readBit(uint8_t reg, uint8_t bit) {
-    return readRegister(reg) & (1 << bit);
+  return readRegister(reg) & (1 << bit);
 }
 
 void BH1745::reset() {
-    setBit(SYSTEM_CONTROL, 7, true);
-    while (readBit(SYSTEM_CONTROL, 7)) {
-        delay(100);
-    }
+  setBit(SYSTEM_CONTROL, 7, true);
+  while (readBit(SYSTEM_CONTROL, 7)) {
+    delay(100);
+  }
 }
 
 void BH1745::getRGBC(uint16_t &r, uint16_t &g, uint16_t &b, uint16_t &c) {
-    uint8_t data[8];
-    readRegisters(COLOUR_DATA, data, 8);
+  uint8_t data[8];
+  readRegisters(COLOUR_DATA, data, 8);
 
-    r = (data[1] << 8) | data[0];
-    g = (data[3] << 8) | data[2];
-    b = (data[5] << 8) | data[4];
-    c = (data[7] << 8) | data[6];
+  r = (data[1] << 8) | data[0];
+  g = (data[3] << 8) | data[2];
+  b = (data[5] << 8) | data[4];
+  c = (data[7] << 8) | data[6];
 
-    if (enableWhiteBalance) {
-        r = r * channelCompensation[0];
-        g = g * channelCompensation[1];
-        b = b * channelCompensation[2];
-        c = c * channelCompensation[3];
-    }
+  if (enableWhiteBalance) {
+    r = r * channelCompensation[0];
+    g = g * channelCompensation[1];
+    b = b * channelCompensation[2];
+    c = c * channelCompensation[3];
+  }
 }
 
 void BH1745::getRGBClamped(uint16_t &r, uint16_t &g, uint16_t &b) {
-    uint16_t c;
-    getRGBC(r, g, b, c);
+  uint16_t c;
+  getRGBC(r, g, b, c);
 
-    uint16_t div = max(r, max(g, b));
+  uint16_t div = max(r, max(g, b));
 
-    if (div > 0) {
-        r = (r * 255) / div;
-        g = (g * 255) / div;
-        b = (b * 255) / div;
-    } else {
-        r = g = b = 0;
-    }
+  if (div > 0) {
+    r = (r * 255) / div;
+    g = (g * 255) / div;
+    b = (b * 255) / div;
+  } else {
+    r = g = b = 0;
+  }
 }
 
 void BH1745::getRGBScaled(uint16_t &r, uint16_t &g, uint16_t &b) {
-    uint16_t c;
-    getRGBC(r, g, b, c);
+  uint16_t c;
+  getRGBC(r, g, b, c);
 
-    if (c > 0) {
-        r = min(255, (r * 255) / c);
-        g = min(255, (g * 255) / c);
-        b = min(255, (b * 255) / c);
-    } else {
-        r = g = b = 0;
-    }
+  if (c > 0) {
+    r = min(255, (r * 255) / c);
+    g = min(255, (g * 255) / c);
+    b = min(255, (b * 255) / c);
+  } else {
+    r = g = b = 0;
+  }
 }
 
 void BH1745::setThresholdHigh(uint16_t value) {
-    writeRegisters(THRESHOLD_HIGH, (uint8_t *)&value, 2);
+  writeRegisters(THRESHOLD_HIGH, (uint8_t *)&value, 2);
 }
 
 void BH1745::setThresholdLow(uint16_t value) {
-    writeRegisters(THRESHOLD_LOW, (uint8_t *)&value, 2);
+  writeRegisters(THRESHOLD_LOW, (uint8_t *)&value, 2);
 }
 
 bool BH1745::setMeasurementTime(uint16_t time_ms) {
-    uint8_t time_reg;
+  uint8_t time_reg;
 
-    switch (time_ms) {
-        case 160:
-            time_reg = 0b000;
-            break;
-        case 320:
-            time_reg = 0b001;
-            break;
-        case 640:
-            time_reg = 0b010;
-            break;
-        case 1280:
-            time_reg = 0b011;
-            break;
-        case 2560:
-            time_reg = 0b100;
-            break;
-        case 5120:
-            time_reg = 0b101;
-            break;
-        default:
-            return false; // Invalid time_ms
-    }
+  switch (time_ms) {
+    case 160:
+      time_reg = 0b000;
+      break;
+    case 320:
+      time_reg = 0b001;
+      break;
+    case 640:
+      time_reg = 0b010;
+      break;
+    case 1280:
+      time_reg = 0b011;
+      break;
+    case 2560:
+      time_reg = 0b100;
+      break;
+    case 5120:
+      time_reg = 0b101;
+      break;
+    default:
+      return false;  // Invalid time_ms
+  }
 
-    writeRegister(MODE_CONTROL1, time_reg);
+  writeRegister(MODE_CONTROL1, time_reg);
 
-    return true;
+  return true;
 }
 
 void BH1745::setLED(bool enable) {
-    setBit(INTERRUPT, 0, enable);
+  setBit(INTERRUPT, 0, enable);
 }
 
 bool BH1745::setADCGain(uint8_t gain) {
-    switch (gain) {
-        case 1:
-            modeControl2.adc_gain = 0b00;
-            break;
-        case 2:
-            modeControl2.adc_gain = 0b01;
-            break;
-        case 16:
-            modeControl2.adc_gain = 0b10;
-            break;
-        default:
-            return false;
-    }
-    return true;
+  switch (gain) {
+    case 1:
+      modeControl2.adc_gain = 0b00;
+      break;
+    case 2:
+      modeControl2.adc_gain = 0b01;
+      break;
+    case 16:
+      modeControl2.adc_gain = 0b10;
+      break;
+    default:
+      return false;
+  }
+  return true;
 }
 
 void BH1745::setWhiteBalance(float r, float g, float b, float c) {
-    channelCompensation[0] = r;
-    channelCompensation[1] = g;
-    channelCompensation[2] = b;
-    channelCompensation[3] = c;
+  channelCompensation[0] = r;
+  channelCompensation[1] = g;
+  channelCompensation[2] = b;
+  channelCompensation[3] = c;
 }
 
 uint8_t BH1745::readRegister(uint8_t reg) {
 #ifdef BH1745_DEBUG
-    Serial.print("Reading register ");
-    Serial.print(reg);
-    Serial.println("…");
+  Serial.print("Reading register ");
+  Serial.print(reg);
+  Serial.println("…");
 #endif
-    Wire.beginTransmission(address);
-    Wire.write(reg);
-    Wire.endTransmission(false);
-    Wire.requestFrom(address, (uint8_t)1);
-    while (!Wire.available()) {
-#if defined(BH1745_DEBUG)
-        Serial.println("Waiting for Wire")
-#endif
-    }
-    uint8_t res = Wire.read();
-    Wire.endTransmission();
+  uint8_t res;
+  if (!readRegisters(reg, &res, 1)) {
+    return -1;
+  }
 #ifdef BH1745_DEBUG
-    Serial.print("Register ");
-    Serial.print(reg);
-    Serial.print(" has the value ");
-    Serial.println(res);
+  Serial.print("Register ");
+  Serial.print(reg);
+  Serial.print(" has the value ");
+  Serial.println(res);
 #endif
-    return res;
+  return res;
 }
 
-void BH1745::readRegisters(uint8_t reg, uint8_t *data, uint8_t length) {
-    Wire.beginTransmission(address);
-    Wire.write(reg);
-    Wire.endTransmission(false);
-    Wire.requestFrom(address, length);
-    for (uint8_t i = 0; i < length; i++) {
-        data[i] = Wire.read();
-    }
-    Wire.endTransmission();
+bool BH1745::readRegisters(uint8_t reg, uint8_t *data, uint8_t length) {
+  uint8_t ord(0);
+  Wire.beginTransmission(address);
+  Wire.write(reg);
+  Wire.endTransmission();
+  Wire.requestFrom(address, length);
+  if (!Wire.available()) {
+#if defined(BH1745_DEBUG)
+    Serial.println("Wire fail!");
+#endif
+    return false;
+  }
+  while (Wire.available()) {
+    data[ord] = Wire.read();
+    ord++;
+  }
+
+  return ord == length;
 }
 
 void BH1745::writeRegisters(uint8_t startReg, uint8_t values[], uint8_t size) {
-    for (uint8_t i = 0; i < size; i++) {
-        uint8_t reg = i + startReg;
-        writeRegister(reg, values[i]);
-    }
+  for (uint8_t i = 0; i < size; i++) {
+    uint8_t reg = i + startReg;
+    writeRegister(reg, values[i]);
+  }
 }
 
 void BH1745::writeRegister(uint8_t reg, uint8_t value) {
 #ifdef BH1745_DEBUG
-    Serial.print("Writing ");
-    Serial.print(value);
-    Serial.print(" to register ");
-    Serial.println(reg);
+  Serial.print("Writing ");
+  Serial.print(value);
+  Serial.print(" to register ");
+  Serial.println(reg);
 #endif
-    Wire.beginTransmission(address);
-    Wire.write(reg);
-    Wire.write(value);
-    Wire.endTransmission();
+  Wire.beginTransmission(address);
+  Wire.write(reg);
+  Wire.write(value);
+  Wire.endTransmission();
 }
 
 uint8_t BH1745::makeModeControl2() {
-    return modeControl2.valid << 7 | modeControl2.rgb_en << 4 | modeControl2.adc_gain;
+  return modeControl2.valid << 7 | modeControl2.rgb_en << 4 | modeControl2.adc_gain;
 }
--- BH1745.h
+++ BH1745.h
@@ -4,11 +4,11 @@
 #include <stdint.h>
 
 struct ModeControl2 {
-    unsigned char valid : 1;
-    unsigned char :2;
-    unsigned char rgb_en : 1;
-    unsigned char :2;
-    unsigned char adc_gain : 2;
+	unsigned char valid : 1;
+	unsigned char : 2;
+	unsigned char rgb_en : 1;
+	unsigned char : 2;
+	unsigned char adc_gain : 2;
 };
 
 /** A singular BH1745 color sensor.
@@ -18,20 +18,20 @@ struct ModeControl2 {
 class BH1745 {
 public:
 	/// Whether to apply the white balance set with setWhiteBalance().
-    bool enableWhiteBalance;
+	bool enableWhiteBalance;
 
 	/** Create the BH1745 class. Optionally takes an address, as the chip supports changing it to 0x39 from the default 0x38.
 	* @param address The chip's I2C bus address.
 	*/
-    BH1745(uint8_t address = 0x38);
+	BH1745(uint8_t address = 0x38);
 
-    /** Establish a connection with the sensor.
+	/** Establish a connection with the sensor.
      * @returns `true` if initialization was successfull.
      */
-    bool init();
+	bool init();
 
-    /// Calls `Wire.begin()` to initialize I2C.
-    static void begin();
+	/// Calls `Wire.begin()` to initialize I2C.
+	static void begin();
 
 	/** Gets semi-raw values from the I2C sensor.
 	* They are adjusted for white balance if `enableChannelCompensation` is true.
@@ -40,7 +40,7 @@ public:
 	* @param &b A pointer to store the blue channel.
 	* @param &c A pointer to store the clear (brightness) channel.
 	*/
-    void getRGBC(uint16_t &r, uint16_t &g, uint16_t &b, uint16_t &c);
+	void getRGBC(uint16_t &r, uint16_t &g, uint16_t &b, uint16_t &c);
 
 	/** Gets relative values from the I2C sensor.
 	* The values are scaled, so that the highest value is now 255.
@@ -58,7 +58,7 @@ public:
 	* @param &g A pointer to store the green channel.
 	* @param &b A pointer to store the blue channel.
 	*/
-    void getRGBClamped(uint16_t &r, uint16_t &g, uint16_t &b);
+	void getRGBClamped(uint16_t &r, uint16_t &g, uint16_t &b);
 
 	/** Gets relative values from the I2C sensor.
 	* The values are scaled, so that the brightness value is now 255.
@@ -69,7 +69,7 @@ public:
 	* @param &g A pointer to store the green channel.
 	* @param &b A pointer to store the blue channel.
 	*/
-    void getRGBScaled(uint16_t &r, uint16_t &g, uint16_t &b);
+	void getRGBScaled(uint16_t &r, uint16_t &g, uint16_t &b);
 
 	/** Sets the "exposure time" of the sensor.
 	* A higher exposure time will saturate the measurements.
@@ -77,20 +77,20 @@ public:
 	*
 	* @param time_ms The "exposure time", in Milliseconds, to set the sensor to.
 	*/
-    bool setMeasurementTime(uint16_t time_ms);
+	bool setMeasurementTime(uint16_t time_ms);
 
 	/** Enables or disables the external pin on the BH1745 chip.
 	* It is for example wired to two LEDs on the Pimoroni breakout.
 	* @param enable The state to set the pin to.
 	*/
-    void setLED(bool enable);
+	void setLED(bool enable);
 
 	/** Sets a fixed multiplier for the chip's Analog-to-Digital-Converter (ADC).
 	* Must be either 1, 2 or 16.
 	*
 	* @param gain The ADC multiplier
 	*/
-    bool setADCGain(uint8_t gain);
+	bool setADCGain(uint8_t gain);
 
 	/** Sets the white balance values. They are multiplied with the raw sensor values if #enableWhiteBalance is true (default).
 	* @param r The compensation to apply to the red channel.
@@ -98,26 +98,26 @@ public:
 	* @param b The compensation to apply to the blue channel.
 	* @param c The compensation to apply to the brightness channel.
 	*/
-    void setWhiteBalance(float r, float g, float b, float c);
+	void setWhiteBalance(float r, float g, float b, float c);
 
-		void setThresholdHigh(uint16_t value);
-		void setThresholdLow(uint16_t value);
+	void setThresholdHigh(uint16_t value);
+	void setThresholdLow(uint16_t value);
 
 private:
 	uint8_t address;
-    float channelCompensation[4];
-    ModeControl2 modeControl2;
+	float channelCompensation[4];
+	ModeControl2 modeControl2;
 
-    uint8_t readRegister(uint8_t reg);
-    void readRegisters(uint8_t reg, uint8_t *data, uint8_t length);
-    void writeRegister(uint8_t reg, uint8_t value);
+	uint8_t readRegister(uint8_t reg);
+	bool readRegisters(uint8_t reg, uint8_t *data, uint8_t length);
+	void writeRegister(uint8_t reg, uint8_t value);
 	void writeRegisters(uint8_t startReg, uint8_t values[], uint8_t size);
-    uint8_t makeModeControl2();
+	uint8_t makeModeControl2();
 
-    void reset();
+	void reset();
 
-    void setBit(uint8_t reg, uint8_t bit, bool newValue);
-    bool readBit(uint8_t reg, uint8_t bit);
+	void setBit(uint8_t reg, uint8_t bit, bool newValue);
+	bool readBit(uint8_t reg, uint8_t bit);
 };
 
 #endif
