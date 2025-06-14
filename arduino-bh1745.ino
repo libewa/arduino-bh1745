3693223fd8f002e454e7b0bddabd573c373b490f
libewa <libewa.git@icloud.com>
2025-06-14 15:07:34 +0200

    Print color data in example sketch

--- arduino-bh1745.ino
+++ arduino-bh1745.ino
@@ -2,6 +2,8 @@
 
 BH1745 colorSensor = BH1745();
 
+uint16_t r, g, b, c;
+
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
     BH1745::begin();
@@ -13,6 +15,7 @@ void setup() {
             delay(950);
         }
     }
+    Serial.begin(9600);
 }
 
 void loop() {
@@ -23,4 +26,9 @@ void loop() {
     colorSensor.setLED(false);
     digitalWrite(LED_BUILTIN, LOW);
     delay(200);
+    colorSensor.getRGBC(r, b, g, c);
+    Serial.println(r);
+    Serial.println(g);
+    Serial.println(b);
+    Serial.println(c);
 }
